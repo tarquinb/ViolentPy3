9cf52619de4f221eee4c399e64496694c4768214
Tarquin <20587979+tarquinb@users.noreply.github.com>
2018-02-27 12:37:22 +0000

    Iterate over IP range

--- bannerGrab.py
+++ bannerGrab.py
@@ -4,33 +4,40 @@ import socket
 from numpy import arange as arange
 
 
-def get_banner(ip, port):
-
-    socket.setdefaulttimeout(2)
-    s = socket.socket()
+def get_banner(s, ip, portlist):
     banner = {}
-    for p in port:
+    for port in portlist:
         try:
-            s.connect((ip, p))
-            banner[p] = s.recv(1024)
-            s.close()
+            print('Connecting to {}:{}\n'.format(str(ip), port))
+            s.connect((ip, port))
+            banner[port] = s.recv(1024)
+            print('Received Banner\n')
+            s.detach()
         except:
             pass
     return banner
 
 
 def main():
-    port = list(arange(1, 10001))
-    ip = '10.10.10.3'
+    portlist = list(arange(1, 100))
+    banner = {}
 
-    banner = get_banner(ip, port)
+    socket.setdefaulttimeout(2)
+    s = socket.socket()
 
-    for key in list(banner.keys()):
-        print('IP: {} PORT: {} \n'.format(ip, key))
-        print(banner[key])
+    for i in arange(1, 5):
+        ip = '10.10.10.' + str(i)
+        banner[ip] = get_banner(s, ip, portlist)
+
+    for ip in list(banner.keys()):
+        for port in banner[ip].keys():
+            print('IP: {} PORT: {} \n'.format(ip, port))
+            print(banner[ip][port])
     return 0
 
 
 if __name__ == "__main__":
-
+    import time
+    start_time = time.time()
     main()
+    print('Time Taken: {} seconds'.format(time.time() - start_time))
